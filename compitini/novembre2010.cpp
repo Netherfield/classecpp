7d1b4539a2c6f37b7aab8a5b7eedc98ceb72b587
Netherfield <barberisjacopo@gmail.com>
2024-11-19 14:58:50 +0100

    esercizio 3 novembre 2010

--- compitini/novembre2010.cpp
+++ compitini/novembre2010.cpp
@@ -18,7 +18,6 @@ Pila operator!(Pila p){
         flip = !flip;
     }
     return q;
-
 }
 
 void read_pila(std::string s, Pila& p){
@@ -40,23 +39,82 @@ void read_pila(std::string s, Pila& p){
 }
 
 
-int main(){
-    std::string spila = "(3, 4, 12, 11, 13, 16, 22)";
-    Pila p, q;
-    read_pila(spila, p);
-    std::cout << p << std::endl;
-    q = !p;
 
-    std::cout << p << "\n" << q << std::endl;    
+class B{
+    public:
+    B(int n) { num = n; }
+    void Set(int n) { num = n; }
+    int Get() const { return num; }
+    private:
+    int num;
+};
 
-    
+class A{
+    public:
+    A(int v = 0) : b(v) { val = v; pb = new B(v); }
+    A(const A&);
+    ~A();
+    void Set1(int h) {
+    val = h; }
+    void Set2(int h) {
+    b.Set(h); }
+    void Set3(int h) {
+    pb->Set(h); }
+    int C1() const { return val; }
+    int C2() const { return b.Get(); }
+    int C3() const { return pb->Get(); }
+    void Flip() { delete pb; pb = &b; }
+    void Flop() { pb = new B(val); }
+    private:
+    int val;
+    B* pb;
+    B b;
+};
+
+A::A(const A& a) : b(a.C2()) {
+    val = a.val;
+    pb = new B(a.C3());
+}
+
+A::~A(){
+    delete pb;
 }
 
 
+namespace std {
+        int test(){
+        // std::string spila = "(3, 4, 12, 11, 13, 16, 22)";
+        // Pila p, q;
+        // read_pila(spila, p);
+        // std::cout << p << std::endl;
+        // q = !p;
 
+        // std::cout << p << "\n" << q << std::endl;
 
+        A a1(2);
+        A a2;
+        // a2.val = 3;
+        a1.Set1(5);
+        // a1.C2() += a1.C1();
+        a1.Set2(a1.C1() + a2.C2() + 1);
+        // a2(5);
+        a1.Flip();
+        cout << a1.C1() << '/' << a1.C2() << '/' << a1.C3() << endl;
 
+        // a2 = a1;
+        A as = a1;
+        a2.Set3(16);
+        cout << a1.C3() << endl;
 
+        return 0; 
+    }
+}
+
+int main(){
+    std::test();
+
+    return 0;
+}
 
 
 
